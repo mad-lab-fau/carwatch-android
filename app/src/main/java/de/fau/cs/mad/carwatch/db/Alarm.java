d633d08889bd9f7b8c64023d87853b7f9618972c
Annika Mücke <annika-muecke@outlook.de>
2022-12-05 16:26:49 +0100

    cleanup unused functions in alarm class

--- app/src/main/java/de/fau/cs/mad/carwatch/db/Alarm.java
+++ app/src/main/java/de/fau/cs/mad/carwatch/db/Alarm.java
@@ -40,16 +40,10 @@ public class Alarm implements Parcelable {
     @ColumnInfo(name = "alarm_active")
     private boolean active;
 
-    // Must have length 7 (= Constants.NUM_DAYS)
-    // i'th item being true means alarm is active on i'th day
-    @ColumnInfo(name = "alarm_active_days")
-    private boolean[] activeDays;
-
     public Alarm() {
         this(
                 Constants.DEFAULT_ALARM_TIME.toDateTimeToday(),
-                false,
-                new boolean[]{false, false, false, false, false, false, false}
+                false
         );
     }
 
@@ -78,22 +72,12 @@ public class Alarm implements Parcelable {
         this.active = active;
     }
 
-    public void setActiveDays(boolean[] activeDays) {
-        this.activeDays = activeDays;
-    }
-
-    public boolean[] getActiveDays() {
-        return activeDays;
-    }
-
 
     // Ignored Members
-
     @Ignore
-    public Alarm(DateTime time, boolean active, boolean[] activeDays) {
+    public Alarm(DateTime time, boolean active) {
         this.time = time;
         this.active = active;
-        this.activeDays = activeDays;
     }
 
     /**
@@ -102,55 +86,6 @@ public class Alarm implements Parcelable {
     @Ignore
     public String getStringTime() {
         return this.time.toString("HH:mm");
-    }
-
-    @Ignore
-    public String getStringOfActiveDays(Context context) {
-        return getStringOfActiveDays(context, activeDays);
-    }
-
-    /**
-     * Get a simple user-readable representation of activeDays
-     *
-     * @param context    The current context
-     * @param activeDays boolean array of days alarm is active
-     * @return a String of alarm's active days
-     */
-    @Ignore
-    public static String getStringOfActiveDays(Context context, boolean[] activeDays) {
-        String[] daysOfWeek = context.getResources().getStringArray(R.array.days_of_week);
-
-        // Build string based on which indices are true in activeDays
-        StringBuilder builder = new StringBuilder();
-        int activeCount = 0;
-        for (int i = 0; i < daysOfWeek.length; i++) {
-            if (activeDays[i]) {
-                String formattedDay = daysOfWeek[i].substring(0, 3) + ", ";
-                builder.append(formattedDay);
-                activeCount++;
-            }
-        }
-
-        if (activeCount == daysOfWeek.length) {
-            return context.getString(R.string.everyday);
-        } else if (activeCount == 0) {
-            return context.getString(R.string.never);
-        }
-
-        boolean satInArray = activeDays[daysOfWeek.length - 2]; // "Saturday" in activeDays
-        boolean sunInArray = activeDays[daysOfWeek.length - 1]; // "Sunday" in activeDays
-
-        if (satInArray && sunInArray && activeCount == 2) {
-            return context.getString(R.string.weekends);
-        } else if (!satInArray && !sunInArray && activeCount == 5) {
-            return context.getString(R.string.weekdays);
-        }
-
-        if (builder.length() > 1) {
-            builder.setLength(builder.length() - 2);
-        }
-
-        return builder.toString();
     }
 
     /**
@@ -163,53 +98,6 @@ public class Alarm implements Parcelable {
         }
 
         return time;
-    }
-
-    /**
-     * Get the time to ring in milliseconds since epoch for each day alarm is active
-     *
-     * @return a List of {@link DateTime} objects to the next alarm ring for each active day
-     */
-    @Ignore
-    public List<DateTime> getTimeToWeeklyRings() {
-        List<DateTime> weekRingTimes = new ArrayList<>();
-
-        for (int i = 0; i < activeDays.length; i++) {
-            if (activeDays[i]) { // if alarm is active on that day
-                weekRingTimes.add(getCorrectRingDay(time.toLocalTime(), i));
-            }
-        }
-
-        return weekRingTimes;
-    }
-
-    /**
-     * Correctly get the next alarm ring day based on the day its active and current day of week
-     *
-     * @param alarmTime {@link LocalTime} since epoch of alarm time **today**
-     * @param activeDay int of day alarm is active on
-     * @return long of milliseconds since epoch of next alarm ring time on active day
-     */
-    @Ignore
-    private DateTime getCorrectRingDay(LocalTime alarmTime, int activeDay) {
-        // JodaTime DateTimeConstants count from 1 (Monday) to 7 (Sunday)
-        activeDay += 1;
-
-        DateTime currTime = DateTime.now();
-        int currDay = currTime.getDayOfWeek();
-
-        DateTime alarmDate = alarmTime.toDateTimeToday();
-
-        // alarm time has passed for today
-        if (alarmDate.isBefore(currTime) || currDay != activeDay) { // current day is not an active day
-            if (activeDay > currDay) { // Alarm is active a later day of the week
-                alarmDate = alarmDate.plusDays(activeDay - currDay);
-            } else { // Have to move the alarm time to next week's active day
-                alarmDate = alarmDate.plusDays(Constants.NUM_DAYS - currDay);
-                alarmDate = alarmDate.plusDays(activeDay);
-            }
-        }
-        return alarmDate;
     }
 
     // Parcelable implementation
@@ -226,7 +114,6 @@ public class Alarm implements Parcelable {
     public void writeToParcel(Parcel out, int flags) {
         out.writeInt(id);
         out.writeLong(DateConverter.toTimestamp(time));
-        out.writeBooleanArray(activeDays);
         out.writeInt(active ? 1 : 0);
     }
 
@@ -249,8 +136,6 @@ public class Alarm implements Parcelable {
         id = in.readInt();
         Long timestamp = in.readLong();
         time = DateConverter.toDate(timestamp);
-        activeDays = new boolean[Constants.NUM_DAYS];
-        in.readBooleanArray(activeDays);
         active = in.readInt() != 0;
     }
 
