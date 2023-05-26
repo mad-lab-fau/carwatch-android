b6afbe672ae2ef5b587316169631c8cf33a6c56e
Annika Mücke <annika-muecke@outlook.de>
2023-05-26 14:58:46 +0200

    reactivate vibration during alarm

--- app/src/main/java/de/fau/cs/mad/carwatch/ui/alarm/ShowAlarmFragment.java
+++ app/src/main/java/de/fau/cs/mad/carwatch/ui/alarm/ShowAlarmFragment.java
@@ -1,6 +1,9 @@
 package de.fau.cs.mad.carwatch.ui.alarm;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import de.fau.cs.mad.carwatch.Constants;
 import de.fau.cs.mad.carwatch.R;
 import de.fau.cs.mad.carwatch.ui.widgets.SwipeButton;
 
@@ -27,8 +31,7 @@ public class ShowAlarmFragment extends Fragment implements SwipeButton.OnSwipeLi
         SwipeButton swipeButton = root.findViewById(R.id.button_swipe);
         swipeButton.setOnSwipeListener(this);
 
-        // TODO removed for testing
-        /*if (getContext() != null) {
+        if (getContext() != null) {
             vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
             if (vibrator != null) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -37,7 +40,7 @@ public class ShowAlarmFragment extends Fragment implements SwipeButton.OnSwipeLi
                     vibrator.vibrate(Constants.VIBRATION_PATTERN, 0);
                 }
             }
-        }*/
+        }
 
 
         return root;
