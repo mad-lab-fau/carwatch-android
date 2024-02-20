18e720745ea962186505a2000bb364e4f83a6a84
roschro-fau <robert.schroeter@fau.de>
2024-02-20 18:12:34 +0100

    added activity for onboarding slide show

---
+++ app/src/main/java/de/fau/cs/mad/carwatch/ui/onboarding/SlideShowActivity.java
@@ -0,0 +1,16 @@
+package de.fau.cs.mad.carwatch.ui.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import de.fau.cs.mad.carwatch.R;
+
+public class SlideShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_show);
+    }
+}
