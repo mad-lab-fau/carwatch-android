5fd31f2c8f8b10d7493e96b562714457cfcd2f0f
roschro-fau <robert.schroeter@fau.de>
2024-02-26 16:34:29 +0100

    light color scheme is applied on app start and renamed function

--- app/src/main/java/de/fau/cs/mad/carwatch/ui/onboarding/SlideShowActivity.java
+++ app/src/main/java/de/fau/cs/mad/carwatch/ui/onboarding/SlideShowActivity.java
@@ -1,11 +1,13 @@
 package de.fau.cs.mad.carwatch.ui.onboarding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -15,6 +17,8 @@ import com.google.android.material.tabs.TabLayout;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.preference.PreferenceManager;
+import de.fau.cs.mad.carwatch.Constants;
 import de.fau.cs.mad.carwatch.R;
 import de.fau.cs.mad.carwatch.ui.MainActivity;
 
@@ -25,6 +29,7 @@ public class SlideShowActivity extends AppCompatActivity {
     private final List<Fragment> slides = new ArrayList<>();
     private final int firstSkippableSlide = 1;
     private int currentSlide = 0;
+    private SharedPreferences sharedPreferences;
     private Button skipButton;
     private Button nextButton;
     private TabLayout tabDots;
@@ -34,10 +39,21 @@ public class SlideShowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide_show);
 
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        setColorScheme();
         initializeSlides();
         initializeSkipButton();
         initializeNextButton();
         initializeCurrentSlideIndicator();
+    }
+
+    private void setColorScheme() {
+        AppCompatDelegate delegate = getDelegate();
+        boolean enableNightMode = sharedPreferences.getBoolean(Constants.PREF_NIGHT_MODE_ENABLED, false);
+        int colorScheme = enableNightMode ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO;
+        AppCompatDelegate.setDefaultNightMode(colorScheme);
+        delegate.applyDayNight();
     }
 
     private void initializeSkipButton() {
@@ -65,7 +81,7 @@ public class SlideShowActivity extends AppCompatActivity {
             tabStrip.getChildAt(i).setOnTouchListener((v, event) -> true);
         }
 
-        selectCurrentTab();
+        highlightCurrentDot();
     }
 
     private void showNextSlide() {
@@ -78,7 +94,7 @@ public class SlideShowActivity extends AppCompatActivity {
             transaction.replace(R.id.slide_show_fragment, slides.get(currentSlide));
             transaction.commit();
 
-            selectCurrentTab();
+            highlightCurrentDot();
 
             if (currentSlide >= firstSkippableSlide) {
                 skipButton.setVisibility(Button.VISIBLE);
@@ -89,7 +105,7 @@ public class SlideShowActivity extends AppCompatActivity {
         }
     }
 
-    private void selectCurrentTab() {
+    private void highlightCurrentDot() {
         TabLayout.Tab tab = tabDots.getTabAt(currentSlide);
         if (tab != null) {
             tab.select();
