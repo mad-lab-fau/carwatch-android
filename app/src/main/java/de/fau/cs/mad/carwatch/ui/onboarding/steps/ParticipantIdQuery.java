fd5e9915ef6bbffbff1c90871e5e914a2b116a3e
roschro-fau <robert.schroeter@fau.de>
2024-02-27 10:43:14 +0100

    added ui fragment for participant id input

---
+++ app/src/main/java/de/fau/cs/mad/carwatch/ui/onboarding/steps/ParticipantIdQuery.java
@@ -0,0 +1,10 @@
+package de.fau.cs.mad.carwatch.ui.onboarding.steps;
+
+import de.fau.cs.mad.carwatch.R;
+
+public class ParticipantIdQuery extends Slide {
+    @Override
+    protected int getResourceId() {
+        return R.layout.fragment_participant_id_query;
+    }
+}
