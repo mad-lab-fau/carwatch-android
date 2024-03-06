87e8abc5ec15a38fc08499ed438330ccdbbce183
Robert Schröter <robert.schroeter@fau.de>
2024-03-06 13:40:24 +0100

    adjusted PermissionRequest texts

--- app/src/main/java/de/fau/cs/mad/carwatch/ui/onboarding/steps/PermissionRequest.java
+++ app/src/main/java/de/fau/cs/mad/carwatch/ui/onboarding/steps/PermissionRequest.java
@@ -1,5 +1,14 @@
 package de.fau.cs.mad.carwatch.ui.onboarding.steps;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
 import de.fau.cs.mad.carwatch.R;
 import de.fau.cs.mad.carwatch.util.Utils;
 
@@ -8,9 +17,27 @@ import de.fau.cs.mad.carwatch.util.Utils;
  */
 public class PermissionRequest extends BaseWelcomeSlide {
 
-    public PermissionRequest() {
-        super();
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         isSkipButtonVisible.set(false);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = super.onCreateView(inflater, container, savedInstanceState);
+
+        if (root == null)
+            return null;
+
+        LinearLayout notificationText = root.findViewById(R.id.ll_notification_permission_description);
+        // notification permission is only required for Android 13 and above
+        // so we hide the notification permission description for older versions
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            notificationText.setVisibility(View.GONE);
+        }
+
+        return root;
     }
 
     @Override
