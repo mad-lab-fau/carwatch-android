40d7b19ea9b08a2c21edb2a32bf851f61ae556b2
roschro-fau <robert.schroeter@fau.de>
2024-03-13 09:23:08 +0100

    removed unused QrActivity.java and widget_participant_id_dialog.xml

--- app/src/main/java/de/fau/cs/mad/carwatch/ui/onboarding/steps/ParticipantIdQuery.java
+++ app/src/main/java/de/fau/cs/mad/carwatch/ui/onboarding/steps/ParticipantIdQuery.java
@@ -1,7 +1,6 @@
 package de.fau.cs.mad.carwatch.ui.onboarding.steps;
 
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,17 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.LinkedHashSet;
-
 import androidx.preference.PreferenceManager;
-import de.fau.cs.mad.carwatch.BuildConfig;
 import de.fau.cs.mad.carwatch.Constants;
 import de.fau.cs.mad.carwatch.R;
-import de.fau.cs.mad.carwatch.logger.LoggerUtil;
-import de.fau.cs.mad.carwatch.util.Utils;
+import de.fau.cs.mad.carwatch.logger.MetadataLogger;
 
 public class ParticipantIdQuery extends BaseWelcomeSlide {
 
@@ -56,92 +48,17 @@ public class ParticipantIdQuery extends BaseWelcomeSlide {
         String participantId = participantIdEditText.getText().toString();
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
         sharedPreferences.edit()
-                .putString(Constants.PREF_SUBJECT_ID, participantId)
-                .putBoolean(Constants.PREF_SUBJECT_ID_IS_SET, false)
+                .putString(Constants.PREF_PARTICIPANT_ID, participantId)
+                .putBoolean(Constants.PREF_PARTICIPANT_ID_WAS_SET, false)
                 .apply();
-        logInitialData(sharedPreferences);
+        MetadataLogger.logDeviceProperties();
+        MetadataLogger.logAppMetadata();
+        MetadataLogger.logStudyData(requireContext());
+        MetadataLogger.logParticipantId(requireContext());
     }
 
     @Override
     protected int getResourceId() {
         return R.layout.fragment_participant_id_query;
-    }
-
-    private void logInitialData(SharedPreferences sharedPreferences) {
-        logParticipantId(sharedPreferences);
-        logAppPhoneMetadata();
-        logStudyData(sharedPreferences);
-    }
-
-    private void logParticipantId(SharedPreferences sharedPreferences) {
-        String participantId = sharedPreferences.getString(Constants.PREF_SUBJECT_ID, "");
-
-        try {
-            JSONObject json = new JSONObject();
-            json.put(Constants.LOGGER_EXTRA_SUBJECT_ID, participantId);
-            LoggerUtil.log(Constants.LOGGER_ACTION_SUBJECT_ID_SET, json);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void logAppPhoneMetadata() {
-        try {
-            // App Metadata – Version Code and Version Name
-            JSONObject json = new JSONObject();
-            json.put(Constants.LOGGER_EXTRA_APP_VERSION_CODE, BuildConfig.VERSION_CODE);
-            json.put(Constants.LOGGER_EXTRA_APP_VERSION_NAME, BuildConfig.VERSION_NAME);
-            LoggerUtil.log(Constants.LOGGER_ACTION_APP_METADATA, json);
-
-            // Phone Metadata – Brand, Manufacturer, Model, Android SDK level, Security Patch (if applicable), Build Release
-            json = new JSONObject();
-            json.put(Constants.LOGGER_EXTRA_PHONE_BRAND, Build.BRAND);
-            json.put(Constants.LOGGER_EXTRA_PHONE_MANUFACTURER, Build.MANUFACTURER);
-            json.put(Constants.LOGGER_EXTRA_PHONE_MODEL, Build.MODEL);
-            json.put(Constants.LOGGER_EXTRA_PHONE_VERSION_SDK_LEVEL, Build.VERSION.SDK_INT);
-            json.put(Constants.LOGGER_EXTRA_PHONE_VERSION_SECURITY_PATCH, Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? Build.VERSION.SECURITY_PATCH : ""); // this
-            json.put(Constants.LOGGER_EXTRA_PHONE_VERSION_RELEASE, Build.VERSION.RELEASE); // this
-
-            LoggerUtil.log(Constants.LOGGER_ACTION_PHONE_METADATA, json);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void logStudyData(SharedPreferences sharedPreferences) {
-        try {
-            // construct human-readable sample ids
-            boolean hasEveningSalivette = sharedPreferences.getBoolean(Constants.PREF_HAS_EVENING, false);
-            String salivaDistancesString = sharedPreferences.getString(Constants.PREF_SALIVA_DISTANCES, "");
-            String salivaTimesString = sharedPreferences.getString(Constants.PREF_SALIVA_TIMES, "");
-            int[] salivaDistances = Utils.decodeArrayFromString(salivaDistancesString);
-            String[] salivaTimes = salivaTimesString.split(Constants.QR_PARSER_LIST_SEPARATOR);
-            String startSample = sharedPreferences.getString(Constants.PREF_START_SAMPLE, "");
-            String samplePrefix = startSample.substring(0, 1);
-            int startSampleIdx = Integer.parseInt(startSample.substring(1));
-            LinkedHashSet<String> salivaIds = new LinkedHashSet<>();
-            for (int i = startSampleIdx; i < salivaDistances.length + startSampleIdx + salivaTimes.length; i++) {
-                String sampleId = samplePrefix + i;
-                salivaIds.add(sampleId);
-            }
-            if (hasEveningSalivette) {
-                salivaIds.add(samplePrefix + Constants.EXTRA_SALIVA_ID_EVENING);
-            }
-            // log all relevant study data
-            JSONObject json = new JSONObject();
-            json.put(Constants.LOGGER_EXTRA_STUDY_NAME, sharedPreferences.getString(Constants.PREF_STUDY_NAME, ""));
-            json.put(Constants.LOGGER_EXTRA_NUM_SUBJECTS, sharedPreferences.getInt(Constants.PREF_NUM_SUBJECTS, 0));
-            json.put(Constants.LOGGER_EXTRA_SALIVA_DISTANCES, salivaDistancesString);
-            json.put(Constants.LOGGER_EXTRA_SALIVA_TIMES, salivaTimesString);
-            json.put(Constants.LOGGER_EXTRA_STUDY_DAYS, sharedPreferences.getInt(Constants.PREF_NUM_DAYS, 0));
-            json.put(Constants.LOGGER_EXTRA_SALIVA_IDS, salivaIds);
-            json.put(Constants.LOGGER_EXTRA_HAS_EVENING_SALIVETTE, hasEveningSalivette);
-            json.put(Constants.LOGGER_EXTRA_SHARE_EMAIL_ADDRESS, sharedPreferences.getString(Constants.PREF_SHARE_EMAIL_ADDRESS, ""));
-            json.put(Constants.LOGGER_EXTRA_CHECK_DUPLICATES, sharedPreferences.getBoolean(Constants.PREF_CHECK_DUPLICATES, false));
-            json.put(Constants.LOGGER_EXTRA_MANUAL_SCAN, sharedPreferences.getBoolean(Constants.PREF_MANUAL_SCAN, false));
-            LoggerUtil.log(Constants.LOGGER_ACTION_STUDY_DATA, json);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 }
