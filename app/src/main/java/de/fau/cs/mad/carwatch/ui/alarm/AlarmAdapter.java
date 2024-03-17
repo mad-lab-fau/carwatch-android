b8115b9c6ba49e251262a2223dae5d15588f1ae3
roschro-fau <robert.schroeter@fau.de>
2024-03-17 19:54:54 +0100

    added "open scanner" icon and "sample finished" icon to alarm list

--- app/src/main/java/de/fau/cs/mad/carwatch/ui/alarm/AlarmAdapter.java
+++ app/src/main/java/de/fau/cs/mad/carwatch/ui/alarm/AlarmAdapter.java
@@ -1,9 +1,11 @@
 package de.fau.cs.mad.carwatch.ui.alarm;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,9 +17,11 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.fau.cs.mad.carwatch.Constants;
 import de.fau.cs.mad.carwatch.R;
 import de.fau.cs.mad.carwatch.alarmmanager.AlarmHandler;
 import de.fau.cs.mad.carwatch.db.Alarm;
+import de.fau.cs.mad.carwatch.ui.BarcodeActivity;
 
 public class AlarmAdapter extends RecyclerView.Adapter<AlarmAdapter.ViewHolder> {
     private final List<Alarm> localAlarms = new ArrayList<>();
@@ -27,12 +31,16 @@ public class AlarmAdapter extends RecyclerView.Adapter<AlarmAdapter.ViewHolder> 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final SwitchMaterial alarmSwitch;
         private final TextView alarmTextView;
+        private final ImageView scannerIcon;
+        private final ImageView checkIcon;
 
         public ViewHolder(View view) {
             super(view);
 
             alarmSwitch = view.findViewById(R.id.alarm_active_switch);
             alarmTextView = view.findViewById(R.id.alarm_time_text);
+            scannerIcon = view.findViewById(R.id.iv_scanner_icon);
+            checkIcon = view.findViewById(R.id.iv_check_icon);
         }
 
         public SwitchMaterial getAlarmSwitch() {
@@ -41,6 +49,14 @@ public class AlarmAdapter extends RecyclerView.Adapter<AlarmAdapter.ViewHolder> 
 
         public TextView getAlarmTextView() {
             return alarmTextView;
+        }
+
+        public ImageView getScannerIcon() {
+            return scannerIcon;
+        }
+
+        public ImageView getCheckIcon() {
+            return checkIcon;
         }
     }
 
@@ -62,6 +78,7 @@ public class AlarmAdapter extends RecyclerView.Adapter<AlarmAdapter.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Alarm item = localAlarms.get(position);
         int colorId = item.isActive() ? R.color.colorAccent : R.color.colorGrey500;
+        setIconProperties(holder, item);
         holder.getAlarmTextView().setText(item.getStringTime());
         holder.getAlarmTextView().setTextColor(resources.getColor(colorId));
         holder.getAlarmSwitch().setChecked(item.isActive());
@@ -82,6 +99,19 @@ public class AlarmAdapter extends RecyclerView.Adapter<AlarmAdapter.ViewHolder> 
     public void setAlarms(List<Alarm> alarms) {
         localAlarms.clear();
         localAlarms.addAll(alarms);
+    }
+
+    private void setIconProperties(@NonNull ViewHolder holder, @NonNull Alarm alarm) {
+        int checkVisibility = alarm.wasSampleTaken() ? View.VISIBLE : View.GONE;
+        int scannerVisibility = alarm.wasSampleTaken() ? View.GONE : View.VISIBLE;
+        holder.getCheckIcon().setVisibility(checkVisibility);
+        holder.getScannerIcon().setVisibility(scannerVisibility);
+        holder.getScannerIcon().setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), BarcodeActivity.class);
+            intent.putExtra(Constants.EXTRA_ALARM_ID, alarm.getId());
+            intent.putExtra(Constants.EXTRA_SALIVA_ID, alarm.getSalivaId());
+            view.getContext().startActivity(intent);
+        });
     }
 
     private void deactivateAlarm(View view, ViewHolder holder, Alarm alarm) {
