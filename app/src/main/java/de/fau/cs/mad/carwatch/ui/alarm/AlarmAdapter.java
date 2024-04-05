d2fe6328d994a6d126d45732118c657b328358ad
roschro-fau <robert.schroeter@fau.de>
2024-04-05 10:19:36 +0200

    renamed file item_fixed_alarm.xml to item_alarm.xml

--- app/src/main/java/de/fau/cs/mad/carwatch/ui/alarm/AlarmAdapter.java
+++ app/src/main/java/de/fau/cs/mad/carwatch/ui/alarm/AlarmAdapter.java
@@ -85,7 +85,7 @@ public class AlarmAdapter extends RecyclerView.Adapter<AlarmAdapter.ViewHolder> 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_fixed_alarm, parent, false);
+                .inflate(R.layout.item_alarm, parent, false);
 
         return new ViewHolder(view);
     }
