a342ba08ee77a8a2c19b1cf2e413fc0d28064482
Robert Schröter <robert.schroeter@fau.de>
2024-04-27 12:03:34 +0200

    light data can now be stored in file

--- app/src/main/java/de/fau/cs/mad/carwatch/sensors/LightIntensityLogger.java
+++ app/src/main/java/de/fau/cs/mad/carwatch/sensors/LightIntensityLogger.java
@@ -10,23 +10,31 @@ import android.util.Pair;
 
 import androidx.annotation.NonNull;
 
+import org.joda.time.DateTime;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import de.fau.cs.mad.carwatch.Constants;
+import de.fau.cs.mad.carwatch.logger.DiskLogHandler;
 import de.fau.cs.mad.carwatch.logger.LoggerUtil;
 
 public class LightIntensityLogger implements SensorEventListener {
 
     private static final String TAG = LightIntensityLogger.class.getSimpleName();
+    private static final String FILE_NAME = "light_intensity_data.csv";
+    private static final String CSV_HEADER = "Date,Time,Light Intensity";
     private static final String UNIT = "lx";
     private static final int PERIOD = 10; // in seconds
     private static LightIntensityLogger instance;
-    private static SensorManager sensorManager;
-    private static boolean isRegistered = false;
+    private SensorManager sensorManager;
+    private boolean isRecording = false;
     private float lightIntensity;
     private long lastLogTime = 0; // in milliseconds
     private List<Pair<Long, Float>> lightIntensityData = new ArrayList<>();
@@ -40,26 +48,22 @@ public class LightIntensityLogger implements SensorEventListener {
         return instance;
     }
 
-    public static LightIntensityLogger startRecording(@NonNull Context context) {
-        if (instance == null)
-            instance = new LightIntensityLogger();
-
-        if (isRegistered)
-            return instance;
+    public void startRecording(@NonNull Context context) {
+        if (isRecording)
+            return;
 
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorManager.registerListener(instance, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        isRegistered = true;
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        isRecording = true;
         Log.d(TAG, "Light sensor registered");
-        return instance;
     }
 
-    public static void stopRecording() {
-        if (!isRegistered)
+    public void stopRecording() {
+        if (!isRecording)
             return;
-        sensorManager.unregisterListener(instance);
-        isRegistered = false;
+        sensorManager.unregisterListener(this);
+        isRecording = false;
         Log.d(TAG, "Light sensor unregistered");
     }
 
@@ -77,6 +81,27 @@ public class LightIntensityLogger implements SensorEventListener {
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) { }
 
+    public void storeLightData(Context context) {
+        try {
+            File file = getLightDataFile(context);
+            FileWriter fw = new FileWriter(file, true);
+
+            for (Pair<Long, Float> data : lightIntensityData) {
+                DateTime time = new DateTime(data.first);
+
+                fw.append(time.toString("yyyy-MM-dd")).append(",");
+                fw.append(time.toString("HH:mm:ss")).append(",");
+                fw.append(data.second.toString()).append(" ").append(UNIT);
+                fw.append("\n");
+            }
+
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Error while writing light data to file", e);
+        }
+    }
+
     public void logLightIntensity() {
         try {
             JSONObject json = new JSONObject();
@@ -86,5 +111,27 @@ public class LightIntensityLogger implements SensorEventListener {
         } catch (JSONException e) {
             LoggerUtil.log(TAG, "current light intensity: " + lightIntensity);
         }
+    }
+
+    private File getLightDataFile(Context context) throws FileNotFoundException {
+        File dir = DiskLogHandler.getLogDirectory(context);
+        if (!dir.exists())
+            throw new FileNotFoundException("Could not create directory");
+
+        File file = new File(dir, FILE_NAME);
+        if (file.exists())
+            return file;
+
+        try {
+            FileWriter fw = new FileWriter(file, true);
+            fw.append(CSV_HEADER);
+            fw.append("\n");
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Error while writing headline for sleep data file", e);
+        }
+
+        return file;
     }
 }
